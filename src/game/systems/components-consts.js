7fb5c091ea02950de46f86891333a2bda916c3c4
Mikhail Remmele <mikhail.remmele@gmail.com>
2023-07-09 18:35:22 +0300

    add some constants to simplify workshop process

---
+++ src/game/systems/components-consts.js
@@ -0,0 +1,5 @@
+// Components
+export const TRANSFORM_COMPONENT_NAME = 'transform';
+
+// Custom components
+export const MOVEMENT_COMPONENT_NAME = 'movement';
---
+++ src/game/systems/messages-consts.js
@@ -0,0 +1,8 @@
+// Messages
+export const COLLISION_ENTER_MSG = 'COLLISION_ENTER';
+export const ADD_IMPULSE_MSG = 'ADD_IMPULSE';
+
+// Custom messages
+export const MOVE_LEFT_MSG = 'MOVE_LEFT';
+export const MOVE_RIGHT_MSG = 'MOVE_RIGHT';
+export const MOVE_JUMP_MSG = 'MOVE_JUMP';
