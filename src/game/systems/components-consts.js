c077cc62f9edd50a996d2ea86e6db867e4fcf56d
Mikhail Remmele <mikhail.remmele@gmail.com>
2023-07-26 19:57:38 +0300

    add additional constants

--- src/game/systems/components-consts.js
+++ src/game/systems/components-consts.js
@@ -1,6 +1,11 @@
 // Components
 export const TRANSFORM_COMPONENT_NAME = 'transform';
 export const RIGID_COMPONENT_NAME = 'rigidBody';
+export const COLLIDER_COMPONENT_NAME = 'colliderContainer';
+export const CONTROL_COMPONENT_NAME = 'keyboardControl';
 
 // Custom components
 export const MOVEMENT_COMPONENT_NAME = 'movement';
+export const AI_COMPONENT_NAME = 'ai';
+export const AI_BLOCKER_COMPONENT_NAME = 'aiBlocker';
+export const ATTACK_COMPONENT_NAME = 'attack';
--- src/game/systems/messages-consts.js
+++ src/game/systems/messages-consts.js
@@ -1,8 +1,10 @@
 // Messages
 export const COLLISION_ENTER_MSG = 'COLLISION_ENTER';
 export const ADD_IMPULSE_MSG = 'ADD_IMPULSE';
+export const LOAD_SCENE_MSG = 'LOAD_SCENE';
 
 // Custom messages
 export const MOVE_LEFT_MSG = 'MOVE_LEFT';
 export const MOVE_RIGHT_MSG = 'MOVE_RIGHT';
 export const MOVE_JUMP_MSG = 'MOVE_JUMP';
+export const ATTACK_MSG = 'ATTACK';
